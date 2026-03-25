70bab372a60b98336cb720bb8a9d5cf8b35430fe
nicolasaurora <nicolas.aurora5@gmail.com>
2026-03-25 17:12:44 -0300

    Agregamos funcionalidades debido a avance del frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const cors = require('cors')
+const app = express()
 
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-const sucursalRoutes = require('./routes/sucursal.routes');
-const barberoRoutes = require('./routes/barbero.routes');
-const servicioRoutes = require('./routes/servicio.routes');
-const registroRoutes = require('./routes/registro.routes');
-const authRoutes = require('./routes/auth.routes');
+const sucursalRoutes = require('./routes/sucursal.routes')
+const barberoRoutes = require('./routes/barbero.routes')
+const servicioRoutes = require('./routes/servicio.routes')
+const registroRoutes = require('./routes/registro.routes')
+const authRoutes = require('./routes/auth.routes')
 
-app.use('/api/sucursales', sucursalRoutes);
-app.use('/api/barberos', barberoRoutes);
-app.use('/api/servicios', servicioRoutes);
-app.use('/api/registros', registroRoutes);
-app.use('/api/auth', authRoutes);
+app.use('/api/sucursales', sucursalRoutes)
+app.use('/api/barberos', barberoRoutes)
+app.use('/api/servicios', servicioRoutes)
+app.use('/api/registros', registroRoutes)
+app.use('/api/auth', authRoutes)
 
 module.exports = app
